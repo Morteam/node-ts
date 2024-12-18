53065986af20855c0e730ddd5cf9bcf56bc2bde4
Manu Castro <mfcastro28@misena.edu.co>
2024-12-17 23:10:11 -0500

    :hammer: chore: axios

--- 02-bases/src/plugins/http-client.plugin.js
+++ 02-bases/src/plugins/http-client.plugin.js
@@ -1,14 +1,30 @@
+// With Axios
+import axios from 'axios';
+
 const httpClientPlugin = {
     get: async (url) => {
-        const response = await fetch(url)
-        const data = await response.json()
-    
-        return data
+        const response = await axios.get(url);
+        const data = response.data;
+
+        return data;
     },
-    post: async() => {},
-    put: async() => {},
-    delete: async() => {},
-}
+    post: async () => {},
+    put: async () => {},
+    delete: async () => {},
+};
+
+// With Fetch
+// const httpClientPlugin = {
+//     get: async (url) => {
+//         const response = await fetch(url)
+//         const data = await response.json()
+    
+//         return data
+//     },
+//     post: async () => {},
+//     put: async () => {},
+//     delete: async () => {},
+// }
 
 export {
     httpClientPlugin,
