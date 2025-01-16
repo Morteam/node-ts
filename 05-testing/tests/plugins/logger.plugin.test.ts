ff27c906baa965f41c7fccb8fd048e69206ada73
Manu Castro <mfcastro28@misena.edu.co>
2025-01-15 21:11:20 -0500

    :hammer: chore: command build and start with good practices

--- 05-testing/tests/plugins/logger.plugin.test.ts
+++ 05-testing/tests/plugins/logger.plugin.test.ts
@@ -12,14 +12,31 @@ describe('plugins/logger', () => {
     test('buildLogger.log should call winston lib', () => {
         const MESSAGE_MOCK = 'Sample test message'
         const SERVICE_NAME_MOCK = 'Sample'
+        const LEVEL = 'info'
 
         const winstonLoggerMock = jest.spyOn(winstonLogger, 'log');
 
         const logger = buildLogger(SERVICE_NAME_MOCK)
         logger.log(MESSAGE_MOCK)
 
-        expect(winstonLoggerMock).toHaveBeenCalledWith('info', expect.objectContaining({ // expect.objectContaining allows us to expect a minimum values without know all values of object
-            level: 'info',
+        expect(winstonLoggerMock).toHaveBeenCalledWith(LEVEL, expect.objectContaining({ // expect.objectContaining allows us to expect a minimum values without know all values of object
+            level: LEVEL,
+            message: MESSAGE_MOCK,
+            service: SERVICE_NAME_MOCK
+        }))
+    })
+
+    test('buildLogger.error should call winston lib', () => {
+        const MESSAGE_MOCK = 'Sample test error'
+        const SERVICE_NAME_MOCK = 'Sample'
+        const LEVEL = 'error'
+
+        const winstonLoggerMock = jest.spyOn(winstonLogger, 'error')
+
+        const logger = buildLogger(SERVICE_NAME_MOCK)
+        logger.error(MESSAGE_MOCK)
+
+        expect(winstonLoggerMock).toHaveBeenCalledWith(LEVEL, expect.objectContaining({
             message: MESSAGE_MOCK,
             service: SERVICE_NAME_MOCK
         }))
