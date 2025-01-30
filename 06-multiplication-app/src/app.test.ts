9349c957d2d078372cf37be2466c82342e1fedbc
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-01-30 08:00:25 -0500

    :hammer: chore: start test multiplication app

---
+++ 06-multiplication-app/src/app.test.ts
@@ -0,0 +1,9 @@
+import {describe, expect, test} from '@jest/globals';
+
+describe('app.ts', () => {
+    test('Should be true', () => {
+        const aaa = true
+
+        expect(aaa).toBe(true)
+    })
+})
