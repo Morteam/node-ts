51d74390944ffe6c471c9f8a07ec62004da734ed
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-02-21 07:52:12 -0500

    :hammer: chore: implements Repository in Use Case

--- 07-noc/src/domain/use-cases/checks/check-service.use-case.ts
+++ 07-noc/src/domain/use-cases/checks/check-service.use-case.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverity } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
+
 interface CheckServiceUseCase {
     execute(url: string): Promise<Boolean>;
 }
@@ -8,6 +11,7 @@ export type ErrorCallback = (error: string) => void
 
 export class CheckService implements CheckServiceUseCase {
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback
     ) {} // Dependencies injection: Manage dependencies between system components in a flexible and decoupled manner, These are injected from the outside, usually through their constructors, methods or properties, avoid adding extra logic to our functions (logic that is not directly related to the main logic of the function)
@@ -18,10 +22,18 @@ export class CheckService implements CheckServiceUseCase {
 
             if(!request.ok) throw new Error('The server is down :(')
 
+            const logMessage = `Service ${url} working`
+            const log = new LogEntity(logMessage, LogSeverity.low)
+
+            this.logRepository.saveLog(log)
             this.successCallback()
 
             return true;
         } catch(error) {
+            const logMessage = `${error}: :(`
+            const log = new LogEntity(logMessage, LogSeverity.high)
+
+            this.logRepository.saveLog(log)
             this.errorCallback(`${error}: :(`)
 
             return false;
