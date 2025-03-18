811c45e80c81a091ed60234ee3d3f1ab4140e2b4
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-03-18 07:28:46 -0500

    :hammer: chore: postgres-log datasource 2

--- 08-email-sending/src/infraestructure/datasources/postgres-log.datadource.ts
+++ 08-email-sending/src/infraestructure/datasources/postgres-log.datadource.ts
@@ -2,6 +2,12 @@ import { PrismaClient, SeverityLevel } from '@prisma/client';
 import { LogDatasource } from '../../domain/datasources/log.datasource';
 import { LogEntity, LogSeverity } from '../../domain/entities/log.entity';
 
+const SeverityEnum = {
+    low: SeverityLevel.LOW,
+    medium: SeverityLevel.MEDIUM,
+    high: SeverityLevel.HIGH
+}
+
 export class PostgresLogDatasource implements LogDatasource {
     prismaClient
     prismaLogModel
@@ -15,7 +21,8 @@ export class PostgresLogDatasource implements LogDatasource {
         const newLog = await this.prismaLogModel.create({
             data: {
                 ...log,
-                level: log.level.toUpperCase() as SeverityLevel, // Type of level of our implementation with Prisma
+                level: SeverityEnum[log.level]
+                // level: log.level.toUpperCase() as SeverityLevel, // Type of level of our implementation with Prisma
             }
         })
 
@@ -24,15 +31,17 @@ export class PostgresLogDatasource implements LogDatasource {
     async getLogs(severityLevel: LogSeverity): Promise<LogEntity[]> {
         const prismaLogs = await this.prismaLogModel.findMany({
             where: {
+                // level: SeverityEnum[severityLevel]
                 level: severityLevel.toUpperCase() as SeverityLevel // Type of level of our implementation with Prisma
             }
         })
 
         // Mapper implementation
+        // const logs = prismaLogs.map(LogEntity.fromObject)
         const logs = prismaLogs.map(prismaLog => LogEntity.fromObject({
             ...prismaLog,
             level: prismaLog.level.toLowerCase() as LogSeverity, // Type of level of our implementation with Node
-        })) // Similar to mongoLogs.map(mongoLog => LogEntity.fromObject(mongoLog))
+        }))
 
         return logs;
     }
