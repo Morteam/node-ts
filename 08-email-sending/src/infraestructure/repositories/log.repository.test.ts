8c00ea55fff04d665fa663b5f40e47bafc81c1c0
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-04-15 07:49:58 -0500

    :hammer: chore: tests Log Repository Implementation

---
+++ 08-email-sending/src/infraestructure/repositories/log.repository.test.ts
@@ -0,0 +1,37 @@
+import { LogDatasource } from '../../domain/datasources/log.datasource';
+import { LogEntity, LogSeverity } from '../../domain/entities/log.entity';
+import { LogRepositoryImpl } from './log.repository';
+
+const dataSourceMocked: LogDatasource = {
+    saveLog: jest.fn(),
+    getLogs: jest.fn(),
+};
+
+const logMocked = {
+    level: LogSeverity.low,
+    message: 'Message says Log mocked',
+    origin: 'fs.datasource.test.ts'
+} as LogEntity;
+
+describe('LogRepositoryImpl', () => {
+
+    test('Should call the saveLog of the datasource', () => {
+        const logRepositoryImplementation = new LogRepositoryImpl(dataSourceMocked);
+        
+        logRepositoryImplementation.saveLog(logMocked)
+
+        expect(logRepositoryImplementation).toBeInstanceOf(LogRepositoryImpl)
+        expect(dataSourceMocked.saveLog).toHaveBeenCalledTimes(1)
+    })
+
+    test('Should call the getLogs of the datasource', () => {
+        const logRepositoryImplementation = new LogRepositoryImpl(dataSourceMocked);
+        
+        logRepositoryImplementation.getLogs(LogSeverity.low)
+
+        expect(dataSourceMocked.getLogs).toHaveBeenCalledTimes(1)
+    })
+
+    // test('Should ', () => {})
+
+})
