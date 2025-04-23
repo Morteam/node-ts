dce630e24dfea569c2273714925d052f228d512a
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-04-22 23:01:09 -0500

    :hammer: chore: my first web server

---
+++ 09-rest-web/info.md
@@ -0,0 +1,3 @@
+## Web Server
+
+Its a software and hardware that allow us serve (get request and response accross a browser) content (like files, websites, text, etc) according to a URL
--- 09-rest-web/src/app.ts
+++ 09-rest-web/src/app.ts
@@ -1,1 +1,20 @@
-console.log('Hi from the moon');
+import http from 'http'; //* Using HTTP
+
+const PORT = 8080
+
+//* Create the web server
+const myFirstWebServer = http.createServer((req, res) => {
+    console.log('Request ', req);
+    console.log('Request ', req.url);
+
+    //* Print
+    res.write(`Hi form the moon ${PORT} and ${req.url}`)
+
+    //* Close the response
+    res.end()
+})
+
+//* Listening the 8080 port in localhost:8080
+myFirstWebServer.listen(PORT, () => {
+    console.log(`The webserver ${PORT} is running`)
+})
