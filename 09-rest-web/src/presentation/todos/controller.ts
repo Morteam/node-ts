55d3344f3ad9095c649545492c40578a08e3870f
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-05-27 20:51:34 -0500

    :hammer: chore: update build command :/

--- 09-rest-web/src/presentation/todos/controller.ts
+++ 09-rest-web/src/presentation/todos/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { prismaClient } from '../../data/postgres/index'
+import { prismaClient } from '../../data/postgres'
 import { CreateTodoDTO, UpdateTodoDTO } from '../../domain/dtos'
 
 
