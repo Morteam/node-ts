2b389e7f96811df93277994c8af5ba1bb1e31e76
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-06-02 08:59:14 -0500

    :hammer: chore: Todo Datasource and Repository

---
+++ 09-rest-web/src/domain/repositories/todo.repository.ts
@@ -0,0 +1,12 @@
+import { CreateTodoDTO, UpdateTodoDTO } from '../dtos';
+import { TodoEntity } from '../entities/todo.entity';
+
+// somehting like an interface. it's a mold
+export abstract class TodoRepository {
+  abstract create(createTodoDTO: CreateTodoDTO): Promise<TodoEntity>
+  // Todo: pagination?
+  abstract getAll(): Promise<TodoEntity[]>
+  abstract getById(id: number): Promise<TodoEntity>
+  abstract updateById(updateTodoDTO: UpdateTodoDTO): Promise<TodoEntity>
+  abstract deleteById(id: number): Promise<TodoEntity>
+}
