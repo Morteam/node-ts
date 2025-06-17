576c8c5b2385bb8f620bf46858403cdebf24f877
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-06-17 07:55:32 -0500

    :hammer: chore: tests App

--- 09-rest-web/src/tests/app.test.ts
+++ 09-rest-web/src/tests/app.test.ts
@@ -1,5 +1,20 @@
+import { envs } from '../config/plugins/envs.plugin';
+import { Server } from '../presentation/server'
+
+jest.mock('../presentation/server') // 1. Mock whole file
+
 describe('App.ts', () => {
-  test('Shoul be truthy', () => {
-    expect(true).toBeTruthy()
+  test('Should call the Server with the arguments and start', async () => {
+    await import('../app') // 2. Import and execute whole file
+
+    // 3. Expecting
+    expect(Server).toHaveBeenCalledTimes(1)
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      publicPath: envs.PUBLIC_PATH,
+      routes: expect.any(Function),
+    })
+
+    expect(Server.prototype.start).toHaveBeenCalledTimes(1)
   })
 })
