9d9fa9ca0ddab3f40024324a4ebf8d54994fbd7b
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-06-24 06:59:30 -0500

    :hammer: chore: Supertest: Get by id

--- 09-rest-web/src/tests/presentation/todos/routes.test.ts
+++ 09-rest-web/src/tests/presentation/todos/routes.test.ts
@@ -8,9 +8,15 @@ describe('Todos routing testing', () => {
     await testServer.start()
   })
 
-  afterAll(async () => {
+  beforeEach(async () => {
+    await prisma.todo.deleteMany()
+  })
+
+  afterAll(() => {
     testServer.close()
   })
+
+  const BASE_ENDPOINT = '/api/todos'
 
   const MOCKED_TODO_1 = {
     text: 'Hi from the Mercury'
@@ -20,11 +26,11 @@ describe('Todos routing testing', () => {
     text: 'Hi from the Venus'
   }
 
+  const MOCKED_TODO_3 = {
+    text: 'Hi from the Earth'
+  }
+
   test('Should return todos api/todos', async () => {
-    const BASE_ENDPOINT = '/api/todos'
-
-    await prisma.todo.deleteMany()
-
     await prisma.todo.createMany({
       data: [MOCKED_TODO_1, MOCKED_TODO_2]
     })
@@ -37,7 +43,22 @@ describe('Todos routing testing', () => {
     expect(body.length).toBe(2)
     expect(body[0].text).toBe(MOCKED_TODO_1.text)
     expect(body[1].text).toBe(MOCKED_TODO_2.text)
+  })
 
+  test('Should return a todo api/todos/:id', async () => {
+    const todo = await prisma.todo.create({
+      data: MOCKED_TODO_3
+    })
+
+    const specificTodo = await request(testServer.getApp)
+      .get(`${BASE_ENDPOINT}/${todo.id}`)
+      .expect(200)
+
+    expect(specificTodo.body).toEqual({
+      id: todo.id,
+      text: MOCKED_TODO_3.text,
+      completedAt: null
+    })
   })
 
 })
