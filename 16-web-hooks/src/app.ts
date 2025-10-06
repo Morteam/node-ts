49cfa1f6e0f21066da999d9f728d653bcd62f2e1
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-10-06 07:27:22 -0500

    :hammer: chore: githubController + NgRok port + Github webhook

--- 16-web-hooks/src/app.ts
+++ 16-web-hooks/src/app.ts
@@ -1,1 +1,25 @@
-console.debug('Hi from the moon')
+import express from 'express'
+
+import { envs } from './config/envs.js'
+import { GithubController } from './presentation/github/controller.js'
+
+(() => {
+  main()
+})()
+
+function main() {
+  const app = express()
+  const githubController = new GithubController()
+
+  app.post('/', (req, res) => {
+    res.json('Path')
+  })
+  app.get('/', (req, res) => {
+    res.json('Path')
+  })
+  app.post('/api/github', githubController.webhookHandler)
+
+  app.listen(envs.PORT, () => {
+    console.log(`App running in port ${envs.PORT}`)
+  })
+}
---
+++ 16-web-hooks/src/presentation/github/controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+
+export class GithubController {
+  constructor(
+
+  ){}
+
+  webhookHandler = (req: Request, res: Response) => {
+    res.json('Done')
+  }
+}
