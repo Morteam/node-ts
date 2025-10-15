e9bb8e06513405cb74d07abde1f2b5c5636cb91b
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-10-15 07:24:59 -0500

    :hammer: chore: Edge Functions: Environment variable

--- 17-serverless-edge-fns/netlify/functions/hello.ts
+++ 17-serverless-edge-fns/netlify/functions/hello.ts
@@ -1,14 +1,8 @@
-// import type { Context } from '@netlify/functions';
-
-// export default async (req: Request, context: Context) => {
-//   return new Response("Hello, world!")
-// }
-
 import type { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
   return {
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World 2" }),
     headers: {
       'Content-Type': 'application/json'
     },
---
+++ 17-serverless-edge-fns/netlify/functions/variables/variables.ts
@@ -0,0 +1,24 @@
+import type { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, context) => {
+  const myImportantVariable = process.env.MY_IMPORTANT_VARIABLE;
+
+  console.log('myImportantVariable ', myImportantVariable)
+
+  if(!myImportantVariable) {
+    throw 'Missing MyImportantVariable'; // Without so much data than new Error()
+  }
+
+  return {
+    body: JSON.stringify({
+      message: "Hello World 2",
+      myImportantVariable,
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    statusCode: 200,
+  }
+}
+
+export { handler }
