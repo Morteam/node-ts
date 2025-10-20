d7ba4df815e34bfd58df9144bf69cc5617ba2b3c
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-10-20 08:00:52 -0500

    :hammer: fix: tsconfig

---
+++ 18-websockets/src/app.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ port: 3000 });
+wss.on('connection', function connection(ws) {
+    console.log('Client Connected');
+    ws.on('error', console.error);
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+    ws.send('Hi from the server!');
+});
+console.log('Port http://localhost:300');
+//# sourceMappingURL=app.js.map
--- 18-websockets/src/app.ts
+++ 18-websockets/src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hi from the moon')
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+  console.log('Client Connected')
+
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('Hi from the server!');
+});
+
+console.log('Port http://localhost:3000')
