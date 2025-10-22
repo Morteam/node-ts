4d175f2eacb5640f4bbd0a58c58fd70eb6b1167e
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-10-22 08:22:13 -0500

    :hammer: chore: WS broadcast (more than on instance=

--- 18-websockets/src/app.ts
+++ 18-websockets/src/app.ts
@@ -1,28 +1,44 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
+  // wss refers to all WS
+  // ws refer to only the instance (Tab browser is only instance)
+
   console.log('WS: Client Connected')
   // console.log('WS ', ws)
 
   ws.on('error', console.error);
 
+  // Only the instance
+  // ws.on('message', function message(data) {
+  //   console.log('WS: received: %s', data);
+
+  //   const payload = {
+  //     type: 'custom-message',
+  //     version: 1,
+  //     data: data.toString(),
+  //   }
+
+  //   ws.send(JSON.stringify(payload))
+  // });
+
   ws.on('message', function message(data) {
     console.log('WS: received: %s', data);
 
-    const payload = {
-      type: 'custom-message',
-      version: 1,
-      data: data.toString(),
-    }
+    wss.clients.forEach((client) => {
+      if(client.readyState === WebSocket.OPEN) {
+        const payload = {
+          type: 'custom-message',
+          version: 1,
+          data: data.toString(),
+        }
 
-    ws.send(JSON.stringify(payload))
+        client.send(JSON.stringify(payload))
+      }
+    })
   });
-
-  // setInterval(() => {
-  //   ws.send('Hi from the server!');
-  // }, 2000)
 
   ws.on('close', () => {
     console.log('Cliente Disconnect')
