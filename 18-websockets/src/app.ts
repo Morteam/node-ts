7322a54d2a2cab60fa8168593936bbb0cfed2851
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-10-22 07:50:25 -0500

    :hammer: chore: WS send message

--- 18-websockets/src/app.ts
+++ 18-websockets/src/app.ts
@@ -3,15 +3,22 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
-  console.log('Client Connected')
+  console.log('WS: Client Connected')
+  // console.log('WS ', ws)
 
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    console.log('WS: received: %s', data);
   });
 
-  ws.send('Hi from the server!');
+  setInterval(() => {
+    ws.send('Hi from the server!');
+  }, 2000)
+
+  ws.on('close', () => {
+    console.log('Cliente Disconnect')
+  })
 });
 
 console.log('Port http://localhost:3000')
