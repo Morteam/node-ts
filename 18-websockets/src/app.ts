5a179bd554b566e4ab44c3fc26b332b5f54b6893
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-10-23 07:47:46 -0500

    :hammer: chore: WS broadcast except itself

--- 18-websockets/src/app.ts
+++ 18-websockets/src/app.ts
@@ -27,10 +27,24 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     console.log('WS: received: %s', data);
 
+    // Send to all clients
+    // wss.clients.forEach((client) => {
+    //   if(client.readyState === WebSocket.OPEN) {
+    //     const payload = {
+    //       type: 'custom-message',
+    //       version: 1,
+    //       data: data.toString(),
+    //     }
+
+    //     client.send(JSON.stringify(payload))
+    //   }
+    // })
+
+    // Send to all clients except itself
     wss.clients.forEach((client) => {
-      if(client.readyState === WebSocket.OPEN) {
+      if(client !== ws && client.readyState === WebSocket.OPEN) {
         const payload = {
-          type: 'custom-message',
+          type: 'custom-message-no-itself',
           version: 1,
           data: data.toString(),
         }
