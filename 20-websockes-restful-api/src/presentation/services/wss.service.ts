1d03fde49ec1fd1721715f1d607cf1715c9e33e1
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-10-30 07:55:15 -0500

    :hammer: chore: Web scoket server

---
+++ 20-websockes-restful-api/src/presentation/services/wss.service.ts
@@ -0,0 +1,40 @@
+import { Server } from 'http'
+import { WebSocketServer, WebSocket } from 'ws';
+
+interface Options {
+  server: Server;
+  path?: string;
+}
+
+export class WSSService {
+  private static _instance: WSSService
+  private wss: WebSocketServer;
+
+  private constructor(options: Options) {
+    const { server, path = '/ws' } = options
+
+    this.wss = new WebSocketServer({ server, path })
+  }
+
+  static get instance(): WSSService {
+    if(!WSSService._instance) {
+      throw 'WSSService is not initialized'
+    }
+
+    return WSSService._instance
+  }
+
+  static initWSServer(options: Options) {
+    WSSService._instance = new WSSService(options)
+  }
+
+  public start() {
+    this.wss.on('connection', (ws: WebSocket) => {
+      console.log('Client Connect')
+
+      ws.on('close', () => {
+        console.log('Client Disconnect')
+      })
+    })
+  }
+}
