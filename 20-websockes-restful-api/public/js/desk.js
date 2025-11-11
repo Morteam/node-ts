84b46244ea8cbf9d380b5c400cff4812f25595bc
ManuelCastroWeb <manuel.castro22@outlook.com>
2025-11-11 07:21:17 -0500

    :hammer: chore: update UI label pending

--- 20-websockes-restful-api/public/js/desk.js
+++ 20-websockes-restful-api/public/js/desk.js
@@ -6,23 +6,40 @@ const loadInitialCount = () => {
     .catch(console.error)
 }
 
-const loadInitialCountAndRender = async (element) => {
-  const initialCount = await loadInitialCount()
+const checkCountPendings = (uiElements, count) => {
+  const { labelPendingEl, noMoreAlert } = uiElements
 
-  element.innerHTML = initialCount.length || 0
+  const HIDE_CSS_CLASS = 'd-none'
+
+  if(count < 1) {
+    noMoreAlert.classList.remove(HIDE_CSS_CLASS)
+    labelPendingEl.classList.add(HIDE_CSS_CLASS)
+  } else {
+    noMoreAlert.classList.add(HIDE_CSS_CLASS)
+    labelPendingEl.classList.remove(HIDE_CSS_CLASS)
+    labelPendingEl.innerHTML = count
+  }
+
+  console.log('AAAAhhh 4')
 }
 
+const loadInitialCountAndRender = async (uiElements) => {
+  const initialCount = await loadInitialCount()
+
+  checkCountPendings(uiElements, initialCount.length || 0)
+}
 
 function connectToWebSockets(socketProps) {
-  const { labelEl } = socketProps
+  const { uiElements } = socketProps
+  const { labelPendingEl } = uiElements
 
   const socket = new WebSocket( 'ws://localhost:3000/ws' );
 
   socket.onmessage = ( event ) => {
     const { type, payload } = JSON.parse(event.data)
-    
+
     if(type === 'on-ticket-count-changed') {
-      labelEl.innerHTML = payload?.pending;
+      checkCountPendings(uiElements, payload?.pending)
     }
   };
 
@@ -30,7 +47,7 @@ function connectToWebSockets(socketProps) {
     console.log( 'Connection closed' );
     setTimeout( () => {
       console.log( 'retrying to connect' );
-      connectToWebSockets();
+      connectToWebSockets(socketProps);
     }, 1500 );
 
   };
@@ -38,16 +55,35 @@ function connectToWebSockets(socketProps) {
   socket.onopen = ( event ) => {
     console.log( 'Connected' );
   };
-
 }
 
-(() => {
-  document.addEventListener('DOMContentLoaded', async () => {
-    const labelPendingEl = document.querySelector('#lbl-pending')
+const updateTitleText = (titleEl, title) => {
+  titleEl.innerText = title
+}
 
-    loadInitialCountAndRender(labelPendingEl)
-    connectToWebSockets({
-      labelEl: labelPendingEl
-    });
-  })
-})()
+const startDOM = async () => {
+  const labelPendingEl = document.querySelector('#lbl-pending')
+  const titleEl = document.querySelector('h1')
+  const noMoreAlert = document.querySelector('.alert')
+
+  const uiElements = {
+    labelPendingEl,
+    titleEl,
+    noMoreAlert
+  }
+
+  const searchParams = new URLSearchParams(window.location.search)
+  
+  if(!searchParams.has('desk')) {
+    window.location = 'index.html'
+    throw new Error('Desk is required')
+  }
+
+  const deskNumber = searchParams.get('desk')
+  updateTitleText(titleEl, deskNumber)
+
+  loadInitialCountAndRender(uiElements)
+  connectToWebSockets({uiElements});
+}
+
+(() => document.addEventListener('DOMContentLoaded', startDOM))()
